d49624a50961b48747aaa4f845ed5a3d0cf62eee
Jon Black <jblack@JolleCocoon.local>
2017-01-17 11:58:00 -0500

    can add todos

--- 7/todos/todotron.rb
+++ 7/todos/todotron.rb
@@ -1,2 +1,10 @@
 class TodoTron
+  attr_reader :items
+  def initialize
+    @items = []
+  end
+  def add_item(description)
+    @items.push(description)
+  end
+
 end
--- 7/todos/todotron_spec.rb
+++ 7/todos/todotron_spec.rb
@@ -1,6 +1,20 @@
 require 'rspec'
 require_relative 'todotron'
+require_relative 'todo'
 
 describe TodoTron do
-  # ...
+  it 'should be able to keep track of multiple todos' do
+    tron = TodoTron.new
+
+    # It has no todo items
+    expect(tron.items.count).to eq 0
+
+    # I add an Item
+    tron.add_item('take the trash out')
+    # tron.items.push(item)
+    # ^ this would make the test drive the data within the object
+
+    # It has one Item
+    expect(tron.items.count).to eq 1
+  end
 end
